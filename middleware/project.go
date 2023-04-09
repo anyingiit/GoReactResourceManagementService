e2daab947631e7e0702ee9e2adf42a7cf5c20b74
anyingiit <49945850+anyingiit@users.noreply.github.com>
2023-04-09 17:02:45 +0800

    reallaze ProjectMustInitialized

--- middleware/project.go
+++ middleware/project.go
@@ -3,9 +3,10 @@ package middleware
 import (
 	"fmt"
 
+	"github.com/anyingiit/GoReactResourceManagement/dao"
 	"github.com/anyingiit/GoReactResourceManagement/globalVars"
-	"github.com/anyingiit/GoReactResourceManagement/models"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // 项目必须初始化
@@ -17,14 +18,15 @@ func ProjectMustInitialized() gin.HandlerFunc {
 			return
 		}
 
-		var count int64
-		if err := db.Model(&models.Sys{}).Count(&count).Error; err != nil {
-			c.Error(err).SetType(gin.ErrorTypePrivate).SetMeta(500)
-			return
-		}
-
-		if count == 0 {
-			c.Error(fmt.Errorf("project must initialized")).SetType(gin.ErrorTypePrivate).SetMeta(500)
+		_, err = dao.FirstSysRecord(db)
+		if err != nil {
+			if err == gorm.ErrRecordNotFound {
+				// 告知项目未初始化
+				c.Error(fmt.Errorf("project must initialized")).SetType(gin.ErrorTypePrivate).SetMeta(500)
+			} else {
+				c.Error(err).SetType(gin.ErrorTypePrivate).SetMeta(500)
+			}
+			c.Abort()
 			return
 		}
 
