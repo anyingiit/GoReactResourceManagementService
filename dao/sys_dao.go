6f9df418f36220b9fd4a21b5253b50a4aa3ffd1b
AnYing <anyingiit@aliyun.com>
2023-05-11 05:05:27 +0800

    使用dao通用函数替换了专有dao函数

--- dao/sys_dao.go
+++ dao/sys_dao.go
@@ -1,13 +1,10 @@
 package dao
 
-import (
-	"github.com/anyingiit/GoReactResourceManagement/models"
-	"gorm.io/gorm"
-)
-
+/* 使用dao通用函数替换了如下专有函数
 // FirstSysRecord
 func FirstSysRecord(db *gorm.DB) (*models.Sys, error) {
 	sys := models.Sys{}
 	err := db.First(&sys).Error
 	return &sys, err
 }
+*/
