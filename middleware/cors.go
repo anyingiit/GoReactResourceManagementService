fe8e335eeecc5a7e7a4b8d83bf2409cc992e6e22
AnYing <anyingiit@aliyun.com>
2023-05-12 02:43:11 +0800

    完善Cors中间件

--- middleware/cors.go
+++ middleware/cors.go
@@ -4,9 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AllowCrossDomain() gin.HandlerFunc {
+func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
 		c.Next()
 	}
 }
