f89a4d7833519b72b541774f7e95f57a7800769e
anyingiit <49945850+anyingiit@users.noreply.github.com>
2023-05-12 02:44:59 +0800

    初步完成superadmin的路由

---
+++ router/v1/superadmin/init.go
@@ -0,0 +1,37 @@
+package superadmin
+
+import (
+	superadminController "github.com/anyingiit/GoReactResourceManagement/controller/superadmin"
+	"github.com/anyingiit/GoReactResourceManagement/middleware"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func InitSuperAdmin(routerGroup *gin.RouterGroup, db *gorm.DB) {
+	baseController := superadminController.NewBaseController(db)
+
+	superadmin := routerGroup.Group("/superadmin", middleware.AuthSuperAdmin())
+	{
+		// resource := superadmin.Group("/:resource", middleware.AllowAllOPTIONS())
+		resource := superadmin.Group("/:resource")
+		{
+			resource.GET("", baseController.GetList)
+			resource.GET("/:id", baseController.GetOne)
+			// resource.GET("/:id/:association", baseController.GetMany)
+			resource.PUT("/:id", baseController.Update)
+			resource.PUT("", baseController.UpdateMany)
+			resource.POST("", baseController.Create)
+			resource.DELETE("/:id", baseController.Delete)
+			resource.DELETE("", baseController.DeleteMany)
+
+		}
+		// superadmin.GET("/:resource", baseController.GetList)
+		// superadmin.GET("/:resource/:id", baseController.GetOne)
+		// // superadmin.GET("/:resource", baseController.GetMany)
+		// superadmin.PUT("/:resource/:id", baseController.Update)
+		// superadmin.PUT("/:resource", baseController.UpdateMany)
+		// superadmin.POST("/:resource", baseController.Create)
+		// superadmin.DELETE("/:resource/:id", baseController.Delete)
+		// superadmin.DELETE("/:resource", baseController.DeleteMany)
+	}
+}
