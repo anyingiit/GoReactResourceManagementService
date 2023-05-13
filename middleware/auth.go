69abcad6af0ba12c44beb6ecbb8b16322e80ae9c
anyingiit <49945850+anyingiit@users.noreply.github.com>
2023-05-14 05:28:50 +0800

    1. 一些小的修复 2. 现在所有和用户相关的接口使用c.Get("user")都会拥有调用着*models.User的值了 3. 增加了客户端的验证中间件

--- middleware/auth.go
+++ middleware/auth.go
@@ -9,10 +9,11 @@ import (
 	"github.com/anyingiit/GoReactResourceManagement/globalVars"
 	"github.com/anyingiit/GoReactResourceManagement/models"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
-func AuthBase(f func(user *models.User)) gin.HandlerFunc {
+func AuthUserBase(f func(user *models.User)) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.Request.Header.Get("Authorization")
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
@@ -43,7 +44,7 @@ func AuthBase(f func(user *models.User)) gin.HandlerFunc {
 		// 此时出现错误，Token是有效的，但是ID是无效的
 		// 只会出现在用户在数据库中被删除后，但是Token未过期的情况下
 
-		if err := dao.First(db, user); err != nil {
+		if err := dao.First(db, user, dao.Preload("Role")); err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
 				// 无法找到用户
 				c.Error(fmt.Errorf("用户已删除")).SetType(gin.ErrorTypePublic).SetMeta(403)
@@ -60,13 +61,15 @@ func AuthBase(f func(user *models.User)) gin.HandlerFunc {
 			return
 		}
 
+		c.Set("user", user)
+
 		f(user)
 	}
 }
 
 func AuthAdmin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		AuthBase(func(user *models.User) {
+		AuthUserBase(func(user *models.User) {
 			if user.Role.Name != "Admin" && user.Role.Name != "SuperAdmin" {
 				c.Error(fmt.Errorf("permission denied")).SetType(gin.ErrorTypePublic).SetMeta(403)
 				c.Abort()
@@ -80,7 +83,7 @@ func AuthAdmin() gin.HandlerFunc {
 
 func AuthUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		AuthBase(func(user *models.User) {
+		AuthUserBase(func(user *models.User) {
 			if user.Role.Name != "Admin" && user.Role.Name != "SuperAdmin" && user.Role.Name != "User" {
 				c.Error(fmt.Errorf("permission denied")).SetType(gin.ErrorTypePublic).SetMeta(403)
 				c.Abort()
@@ -94,7 +97,7 @@ func AuthUser() gin.HandlerFunc {
 
 func AuthSuperAdmin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		AuthBase(func(user *models.User) {
+		AuthUserBase(func(user *models.User) {
 			if user.Role.Name != "SuperAdmin" {
 				c.Error(fmt.Errorf("permission denied")).SetType(gin.ErrorTypePublic).SetMeta(403)
 				c.Abort()
@@ -103,5 +106,51 @@ func AuthSuperAdmin() gin.HandlerFunc {
 
 			c.Next()
 		})(c)
+	}
+}
+
+func AuthClient() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Client-UUID")
+		if len(authHeader) == 0 {
+			c.Error(fmt.Errorf("uuid is empty")).SetType(gin.ErrorTypePublic).SetMeta(401)
+			c.Abort()
+			return
+		}
+		uuid, err := uuid.Parse(authHeader)
+		if err != nil {
+			c.Error(fmt.Errorf("uuid type error")).SetType(gin.ErrorTypePublic).SetMeta(401)
+			c.Abort()
+			return
+		}
+
+		db, err := globalVars.Db.Get()
+		if err != nil {
+			c.Error(err).SetType(gin.ErrorTypePrivate)
+			c.Abort()
+			return
+		}
+
+		clientSession := &models.ClientSession{
+			UUID: uuid,
+		}
+		err = dao.First(db, clientSession)
+		if err != nil {
+			c.Error(err).SetType(gin.ErrorTypePrivate)
+			c.Abort()
+			return
+		}
+
+		client := &models.Client{}
+		err = dao.First(db, client, dao.ByField("id", clientSession.ClientID))
+		if err != nil {
+			c.Error(err).SetType(gin.ErrorTypePrivate)
+			c.Abort()
+			return
+		}
+
+		c.Set("client", client)
+
+		c.Next()
 	}
 }
