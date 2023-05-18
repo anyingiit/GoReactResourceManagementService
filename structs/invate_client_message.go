22933bc798610e73f539cd9128e82469d0434fad
AnYing <anyingiit@aliyun.com>
2023-05-18 18:02:23 +0800

    InvateClientMessage

--- structs/invate_client_message.go
+++ structs/invate_client_message.go
@@ -1,7 +1,7 @@
 package structs
 
 type InvateClientMessage struct {
-	ServerIP   string
-	ServerPort int
-	InvateCode string // 本身是无意义的字符，被服务器验证成功后会返回一个和服务器的通信凭证
+	ServerHost string `json:"server_host"`
+	ServerPort int    `json:"server_port"`
+	InvateCode string `json:"invate_code"` // 本身是无意义的字符，被服务器验证成功后会返回一个和服务器的通信凭证
 }
