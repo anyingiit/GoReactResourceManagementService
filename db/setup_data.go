1d03804b47684b5b45cee0680af0d0c2e315a214
anyingiit <49945850+anyingiit@users.noreply.github.com>
2023-05-18 18:02:52 +0800

    新增一些默认数据

--- db/setup_data.go
+++ db/setup_data.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 
+	"github.com/anyingiit/GoReactResourceManagement/dao"
 	"github.com/anyingiit/GoReactResourceManagement/models"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
@@ -19,6 +20,7 @@ func SetupData(db *gorm.DB, superAdminPassword string) (*SetupDataResult, error)
 	// 1. 创建Sys表中的唯一一条记录
 	// 2. 创建Role表中的默认数据
 	// 3. 创建一个默认的SuperAdmin用户
+	// 4. 创建默认支持的任务
 
 	// 创建Sys表中的唯一一条记录
 	sys := models.Sys{}
@@ -68,7 +70,17 @@ func SetupData(db *gorm.DB, superAdminPassword string) (*SetupDataResult, error)
 
 	// 创建默认支持的任务
 	tasks := []models.Task{}
+	tasks = append(tasks, models.Task{Name: "context_task_queue_pull", Description: "context_task_queue_pull"})
+	tasks = append(tasks, models.Task{Name: "context_task_queue_clear", Description: "context_task_queue_clear"})
+	tasks = append(tasks, models.Task{Name: "context_task_queue_reload", Description: "context_task_queue_reload"})
+	tasks = append(tasks, models.Task{Name: "context_init", Description: "context_init"})
+	tasks = append(tasks, models.Task{Name: "context_client_config_is_validate", Description: "context_client_config_is_validate"})
+	tasks = append(tasks, models.Task{Name: "context_invate_info_load_with_command_line_param", Description: "context_invate_info_load_with_command_line_param"})
+	tasks = append(tasks, models.Task{Name: "client_regist_with_invate_info", Description: "client_regist_with_invate_info"})
+	tasks = append(tasks, models.Task{Name: "update_config_file_with_context", Description: "update_config_file_with_context"})
 	tasks = append(tasks, models.Task{Name: "ping", Description: "Ping a host"})
+	tasks = append(tasks, models.Task{Name: "wait_five_minute", Description: "wait_five_minute"})
+	tasks = append(tasks, models.Task{Name: "contextl_task_queue_result_push", Description: "contextl_task_queue_result_push"})
 	if err := dao.Create(db, &tasks); err != nil {
 		return nil, fmt.Errorf("failed to create tasks")
 	}
