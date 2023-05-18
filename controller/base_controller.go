ee3b9b92e4cef771ee6b2e5cde4dfdaf44855da2
AnYing <anyingiit@aliyun.com>
2023-05-18 18:00:42 +0800

    增加基础请求方法

--- controller/base_controller.go
+++ controller/base_controller.go
@@ -1,6 +1,12 @@
 package controller
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type BaseController struct {
 	Db *gorm.DB
@@ -8,4 +14,25 @@ type BaseController struct {
 
 func NewBaseController(db *gorm.DB) *BaseController {
 	return &BaseController{Db: db}
+}
+
+// Set Content-Range
+func (bc *BaseController) SetContentRange(c *gin.Context, page, perPage int, total int64) {
+	c.Header("Access-Control-Expose-Headers", "Content-Range")
+
+	start := (page-1)*perPage + 1
+	end := page * perPage
+
+	c.Header("Content-Range", fmt.Sprintf("items %d-%d/%d", start, end, total))
+}
+
+// ParseParamID
+func (bc *BaseController) ParseParamID(c *gin.Context) (id uint, err error) {
+	uint64ID, err := strconv.ParseUint(c.Param("id"), 10, 0)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return uint(uint64ID), nil
 }
