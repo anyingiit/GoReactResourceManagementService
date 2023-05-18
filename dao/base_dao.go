8a1ae93c3a6ede8f446a06f00814f0da62234c7f
AnYing <anyingiit@aliyun.com>
2023-05-18 18:02:03 +0800

    增加一些scope函数

--- dao/base_dao.go
+++ dao/base_dao.go
@@ -1,9 +1,10 @@
 package dao
 
 import (
-	"fmt"
+	"strings"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 // ScopeFunc 作用域函数的类型
@@ -12,7 +13,7 @@ type ScopeFunc func(*gorm.DB) *gorm.DB
 // ByField 通过字段名和字段值生成作用域函数，限定查询结果
 func ByField(field string, value interface{}) ScopeFunc {
 	return func(db *gorm.DB) *gorm.DB {
-		return db.Where(fmt.Sprintf("%s = ?", field), value)
+		return db.Where(clause.Eq{Column: clause.Column{Name: field}, Value: value})
 	}
 }
 
@@ -27,6 +28,69 @@ func ByRecordRange(start, end int) ScopeFunc {
 		offset := start - 1
 		limit := end - start + 1
 		return db.Offset(offset).Limit(limit)
+	}
+}
+
+// ByRecordRangeWithPage 通过页码和每页记录数量生成作用域函数，限定查询结果
+func ByRecordRangeWithPage(page, perPage int) ScopeFunc {
+	return ByRecordRange((page-1)*perPage+1, page*perPage)
+}
+
+// Preload 函数实现了预载入关联数据的功能，并返回一个通用的作用域（scope）函数，可以在查询时使用。
+func Preload(scope string) ScopeFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Preload(scope)
+	}
+}
+
+// FieldIn 通过字段名和字段值列表生成作用域函数，限定查询结果
+// 例如：FieldIn("id", []uint{1, 2, 3}) 生成的作用域函数为：db.Where("id IN ?", []uint{1, 2, 3})
+func FieldIn(field string, values []interface{}) ScopeFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Where(clause.IN{Column: clause.Column{Name: field}, Values: values})
+	}
+}
+
+// IDsIn 通过 ID 列表生成作用域函数，限定查询结果
+func IDsIn(ids []uint) ScopeFunc {
+
+	interfaceValues := make([]interface{}, len(ids))
+	for i, val := range ids {
+		interfaceValues[i] = val
+	}
+
+	return FieldIn("id", interfaceValues)
+}
+
+// Joins
+func Joins(query string, args ...interface{}) ScopeFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Joins(query, args...)
+	}
+}
+
+// Order
+func Order(sortField, sortOrder string) ScopeFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		// return db.Order(gorm.Expr("? ?", sortField, sortOrder))
+		return db.Order(clause.OrderByColumn{Column: clause.Column{Name: sortField}, Desc: strings.ToUpper(sortOrder) == "DESC"})
+	}
+}
+
+// ByFields
+func ByFields(fields map[string]interface{}) ScopeFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		for k, v := range fields {
+			db = ByField(k, v)(db)
+		}
+		return db
+	}
+}
+
+// CountScope
+func CountScope(count *int64) ScopeFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Count(count)
 	}
 }
 
